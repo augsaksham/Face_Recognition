f539e484f81e029ce31e288bb374f47a17364975
Tarun-108 <tarunstu125@gmail.com>
2022-05-08 16:45:57 +0530

    main activity xml modified

--- app/src/main/java/com/example/face_recognition/MainActivity.java
+++ app/src/main/java/com/example/face_recognition/MainActivity.java
@@ -371,7 +371,7 @@ public class MainActivity extends AppCompatActivity implements ImageReader.OnIma
 
                 databaseReference.setValue(map);
                 Log.d("downloadUrl-->", "" + downloadUrl);
-                
+
             }
         });
 
